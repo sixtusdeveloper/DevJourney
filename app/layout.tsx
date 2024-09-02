9b30668c896ba69095892ab5bf868655eeb6e44f
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-02 21:55:45 +0100

    commit on Layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "DevJourney Blog",
+  title: "DevJourney Blog app",
   description: "A blog about my journey as a Software Engineer",  
 };
 
